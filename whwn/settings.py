0db74ea9089952630365aaca49b2c8ea253c0011
jnwng <j@jnwng.com>
2013-10-14 22:32:32 -0700

    Adjusting database credentials and setup

--- whwn/settings.py
+++ whwn/settings.py
@@ -15,8 +15,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'wehaveweneed',                      # Or path to database file if using sqlite3.
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'whwn',
+        'PASSWORD': 'whwn',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
