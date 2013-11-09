e31b4f15abe7fcb96159ab29b6e6fafffbfb8b1c
Lewis Chung <lewis.f.chung@gmail.com>
2013-11-09 13:26:21 -0800

    ignore

--- settings.py
+++ settings.py
@@ -134,15 +134,13 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'django_nose',
     'south',
     'whwn',
     'tastypie',
     'compressor',
-    'django_nose',
 )
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
