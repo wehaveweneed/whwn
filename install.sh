2e6776f171c695b7f3832747b278979c4b58aa8f
Lewis Chung <lewis.f.chung@gmail.com>
2013-11-10 17:37:53 -0800

    Cleaned up the code using functions for colored echoing. Added some minor fixes for OSX based PostgreSQL installations

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # install.sh - helping people bootstrap their environments for whwn dev
 
-# Text color variables
+# Text color variables.
 txtund=$(tput sgr 0 1)          # Underline
 txtbld=$(tput bold)             # Bold
 bldred=${txtbld}$(tput setaf 1) #  red
@@ -16,8 +16,16 @@ pass=${bldblu}*${txtrst}
 warn=${bldred}*${txtrst}
 ques=${bldblu}?${txtrst}
 
-# setup by checking which platform we are running.
-if [[ $OSTYPE =~ darwin.* ]] ; then
+function echobr { echo "${bldred}$1${txtrst}" }
+function echobg { echo "${bldgrn}$1${txtrst}" }
+function echobb { echo "${bldblu}$1${txtrst}" }
+function echoby { echo "${bldylw}$1${txtrst}" }
+function echobw { echo "${bldwht}$1${txtrst}" }
+
+########################
+# DETECT WHAT PLATFORM #
+########################
+if [[ $OSTYPE =~ darwin ]] ; then
   PLATFORM="osx"
 elif [[ $OSTYPE =~ linux-gnu ]] ; then
   if [[ `cat /proc/version` =~ .*Ubuntu.* ]] ; then
@@ -27,63 +35,83 @@ elif [[ $OSTYPE =~ linux-gnu ]] ; then
   fi
 fi
 
-# export current dudes environment so ./install.sh has the same environment
+
+# We want to make sure this script runs in the same environment
+# that the user has so we wanna source the files
 if [[ -f $HOME/.bashrc ]] ; then
   . $HOME/.bashrc
 fi
 if [[ -f $HOME/.zshrc ]] ; then
   . $HOME/.zshrc
 fi
+if [[ -f $HOME/.profile ]] ; then
+  . $HOME/.profile
+fi
 
 echo ""
 
-echo "${bldwht}You're going to need Python 2.7 to run this baby... ${txtrst}"
+# Step 1: Check for the correct Python version.
+# TODO: Platform specific installation instructions.
+
+echobw "You're going to need Python 2.7 to run this baby... "
 PYTHON_VERSION=`python -c "import sys; sys.stdout.write(sys.version)" | head -n1 | grep -oe "\d\.\d\.\d"`
 if [[ $PYTHON_VERSION =~ ^[[:digit:]].[[:digit:]].[[:digit:]]$ ]] ; then
-  echo "    ${bldgrn}Version $PYTHON_VERSION found. Nice.${txtrst}"
+  if [[ $PYTHON_VERSION =~ ^2.7.[[:digit:]]$ ]] ; then
+    echobg "    Version $PYTHON_VERSION found. Nice."
+  elif [[ $PYTHON_VERSION =~ ^2.[[:digit:]].[[:digit:]]$ ]] ; then
+    echobr "    You need to upgrade to version 2.7 of Python."
+  fi
 else
-  echo "    ${bldred}No python found :( you need to install Python version 2.7.*${txtrst}"
+  echobr "    No python found :( you need to install Python version 2.7.*"
   exit 1
 fi
 
 echo ""
 
-echo "${bldwht}We use PostgreSQL as our main data store. Let's see if you have it... ${txtrst}"
+# Step 2: Check for a correct PostgreSQL installation.
+# TODO: Platform specific installation instructions.
+
+echobw "We use PostgreSQL as our main data store. Let's see if you have it... "
 POSTGRESQL_VERSION=`psql --version | cut -d" " -f3`
 if [[ $POSTGRESQL_VERSION =~ ^[[:digit:]].[[:digit:]].[[:digit:]]$ ]] ; then
-  echo "    ${bldgrn}Version $POSTGRESQL_VERSION found. Hot stuff.${txtrst}"
+  echobg "    Version $POSTGRESQL_VERSION found. Hot stuff."
 else
-  echo "    ${bldred}You will need to have PostgreSQL installed and running to continue.${txtrst}"
+  echobr "    You will need to have PostgreSQL installed and running to continue."
   exit 1
 fi
 
 echo ""
 
-echo "${bldwht}You're going to need pip to install virtualenv to keep your Python workspace clean...${txtrst}"
+# Step 3: Check for a global installation of pip.
+# TODO: Platform specific installation instructions.
+echobw "You're going to need pip to install virtualenv to keep your Python workspace clean..."
 PIP_VERSION=`pip -V | cut -d" " -f2`
 if [[ $PIP_VERSION =~ ^[[:digit:]].[[:digit:]].[[:digit:]]$ ]] ; then
-  echo "    ${bldgrn}Version $PIP_VERSION found. Sweet.${txtrst}"
+  echobg "    Version $PIP_VERSION found. Sweet."
 else
-  echo "    ${bldred}You will need to install pip to continue.${txtrst}"
+  echobr "    You will need to install pip to continue."
   exit 1
 fi
 
 echo ""
 
-echo "${bldwht}Now that you have pip, you're going to need virtualenv and virtualenvwrapper!${txtrst}"
+# Step 4: Check for an insatllation of virtualenv and virtualenvwrapper
+echobw "Now that you have pip, you're going to need virtualenv and virtualenvwrapper!"
 if [[ -n $(pip list | grep -oe "^virtualenv\s") ]] ; then
-  echo "    ${bldgrn}Perfect, you have virtualenv.${txtrst}"
+  echobg "    Perfect, you have virtualenv."
 else
-  echo "    ${bldred}Looks like you have some work to do. ${txtrst} "
-  echo "    Please install virtualenv and virtualenvwrapper using:"
-  echo "        pip install virtualenv virtualenvwrapper"
+  echobr "    Looks like you have some work to do. "
+  echoby "    Please install virtualenv and virtualenvwrapper using:"
+  echoby "        sudo pip install virtualenv virtualenvwrapper"
   exit 1
 fi
 
 echo ""
 
+# Step 5: Set up the guys virtualenv/virtualenvwrapper .dotfiles if they
+# don't exist already.
 touch ~/.profile
-echo "${bldwht}Checking to see if virtualenvwrapper is set up properly.${txtrst}"
+echobw "Checking to see if virtualenvwrapper is set up properly."
 if [[ -z $(echo $WORKON_HOME) ]] ; then
   echo "    Setting it up for you for you. Running: "
   echo '        echo "export WORKON_HOME=\$HOME/.virtualenvs" >> ~/.profile'
@@ -110,70 +138,93 @@ if [[ -z $(echo $VIRTUALENVWRAPPER_PYTHON) ]] ; then
   # Doing the line add
   echo "export VIRTUALENVWRAPPER_PYTHON=`which python`" >> ~/.profile
 fi
-echo "    ${bldgrn}Virtualenvwrapper is now setup properly. Yes!${txtrst}"
+echobg "    Virtualenvwrapper is now setup properly. Yes!"
 . ~/.profile
 
 echo ""
 
-echo "${bldwht}We use NodeJS and npm to bootstrap our javascript packages.${txtrst}"
+# Step 6: Check for an installation of NodeJS and NPM
+echobw "We use NodeJS and npm to bootstrap our javascript packages."
 if [ -n $(which npm) ] ; then
-  echo "    ${bldgrn}Npm installation found, moving on!.${txtrst}"
+  echobg "    Npm installation found, moving on!"
 else
-  echo "${bldred}Install version 0.10.* of Node to get npm${txtrst}"
+  echobr "    Install version 0.10.* of Node to get npm."
+  echoby "    The suggested method is via Node Version Manager(NVM) which you can find here: "
+  echoby "    https://github.com/creationix/nvm "
+  echoby "    Installation instructions are in the README."
   exit 1
 fi
 
 echo ""
 
-sleep 1
-
+# Step 7: If virtualenv is properly installed, we automatically do everything else
+# for them.
 if [[ `echo $VIRTUAL_ENV` =~ whwn ]] ; then
-  echo "${bldwht}Cool, we'll do the rest from here in this order: ${txtrst}"
-  echo "    ${bldblu}1. Use pip to install our Django project requirements"
-  echo "    2. Setup our PostgreSQL database to properly work"
-  echo "    3. Sync and Migrate our Django project with our PostgreSQL Database"
-  echo "    4. Install the requisite javascript packages using npm and bower.${txtrst}"
+  echobw "Cool, we'll do the rest from here in this order:"
+  echobb "    1. Use pip to install our Django project requirements"
+  echobb "    2. Setup our PostgreSQL database to properly work"
+  echobb "    3. Sync and Migrate our Django project with our PostgreSQL Database"
+  echobb "    4. Install the requisite javascript packages using npm and bower."
 else
   # Instruct to create a virtualenv
-  echo "${bldrd}You're going to need a virtualenv so that you don't crowd your default 
-  system packages.${txtrst}"
+  echobr "You're going to need a virtualenv so that you don't crowd your default 
+  system packages."
 
-  echo "${bldwht}Please run the following to create a whwn environment and: ${txtrst}"
-  echo "    ${bldylw}source ~/.profile    # Reload your ~./profile settings we just added."
-  echo "    mkvirtualenv --no-site-packages --distribute whwn"
-  echo "    workon whwn${txtrst}"
+  echobw "Please run the following to create a whwn environment and: "
+  echoby "    source ~/.profile    # Reload your ~./profile settings we just added."
+  echoby "    mkvirtualenv --no-site-packages --distribute whwn"
+  echoby "    workon whwn"
   echo ""
-  echo "${bldwht}Then rerun ${bldblu}install.sh${bldwht} to continue${txtrst}"
+  echobw "Then rerun ${bldblu}install.sh${bldwht} to continue."
   exit 1
 fi
 
 sleep 3
-
 echo ""
-echo "${bldylw}Running:${bldwht} pip install -r requirements.txt${txtrst}"
 
+# Step 8: We're going to install our python packages using pip.
+echoby "Running:${bldwht} pip install -r requirements.txt"
 workon whwn # load our virtualenv so pip installs into it.
 pip install -r requirements.txt
 echo ""
-echo "${bldylw}Running:${bldwht} npm run-script setup${txtrst}"
+
+# Step 9: We're going to use our npm setup script to install our npm/bower packages.
+echoby "Running:${bldwht} npm run-script setup"
 npm run-script setup
-echo "${bldylw}Running:${bldwht} psql -c \"CREATE USER whwn WITH PASSWORD 'whwn'\" -U postgres
-               psql -c \"CREATE DATABASE wehaveweneed WITH OWNER whwn\" -U postgres
-               psql -c \"ALTER USER whwn CREATEDB\" -U postgres${txtrst}"
+echo ""
+
+# We're going to setup postgres for them.
+
+echoby "Running:${bldwht} psql -c \"CREATE USER whwn WITH PASSWORD 'whwn'\" -U postgres"
+echobw "         psql -c \"CREATE DATABASE wehaveweneed WITH OWNER whwn\" -U postgres"
+echobw "         psql -c \"ALTER USER whwn CREATEDB\" -U postgres"
+echo ""
+
+if [[ $PLATFORM == "osx" ]] ; then
+   echoby "Seems like your platform is osx, which doesn't have a 'postgres' user on most"
+   echoby "default installations. Going to try to create the 'postgres' user for you."
+   echoby "Running: ${bldwht}createdb"
+   echobw "         psql -c \"CREATE USER postgres SUPERUSER CREATEROLE CREATEDB\""
+
+   createdb
+   psql -c "CREATE USER postgres SUPERUSER CREATEROLE CREATEDB"
+fi
+
 psql -c "CREATE USER whwn WITH PASSWORD 'whwn'" -U postgres
 psql -c "CREATE DATABASE wehaveweneed WITH OWNER whwn" -U postgres
 psql -c "ALTER USER whwn CREATEDB" -U postgres
 
 echo ""
-echo "${bldylw}Running:${bldwht} python manage.py syncdb --noinput${txtrst}"
+echoby "Running:${bldwht} python manage.py syncdb --noinput"
 python manage.py syncdb --noinput
 
 echo ""
-echo "${bldylw}Running:${bldwht} python manage.py migrate --noinput${txtrst}"
+echoby "Running:${bldwht} python manage.py migrate --noinput"
 python manage.py migrate --noinput
 
-echo "${bldgrn}Congratulations! Everything is setup! Please peruse the output of this script to know exactly
-what was run.${txtrst}"
+echo ""
+echobg "Congratulations! Everything is setup! Please peruse the output of this script to
+understand exactly what was run."
 exit 0
 
 # DONE! YIPEE!
